aafd2a41adda86f8bf8df35f0e6f78e0d579f63f
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-28 15:35:52 +0600

    events is up

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+// Connect MongoDB
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qizpe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const eventCollection = client.db('be_volunteer').collection('events');
+
+        app.get('/events', async (req, res) => {
+            const query = {};
+            const cursor = eventCollection.find({});
+            const events = await cursor.toArray();
+            res.send(events);
+        })
+
+    }
+    finally { };
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
     res.send('Welcome to Be_Volunteer')
 })
